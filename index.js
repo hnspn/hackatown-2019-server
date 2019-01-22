e0ce701b86017b35f2e4e7259dc2ef6d6cf592e8
hnspn <bobmatt911@gmail.com>
2019-01-22 14:24:15 -0500

    cleaned up a bit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require("express");
-const cors = require("cors");
-var bodyParser = require("body-parser");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const MongoClient = require("mongodb").MongoClient;
+const { MongoClient } = require('mongodb');
 
-const url = "mongodb://marcus:123456a@ds161764.mlab.com:61764/hackatown2019";
+const dbUsername = 'marcus';
+const dbPassword = '123456a';
+const dbName = 'hackatown2019';
+const url = `mongodb://${dbUsername}:${dbPassword}@ds161764.mlab.com:61764/${dbName}`;
+
 let db;
 const app = express();
 app.use(cors());
@@ -12,50 +16,50 @@ app.use(bodyParser.json());
 
 MongoClient.connect(
   url,
-  function(err, client) {
+  (err, client) => {
     db = client;
-  }
+  },
 );
 
 const port = process.env.PORT || 3000;
 
-app.get("/locations", (req, res) => {
+app.get('/locations', (req, res) => {
   db.db()
-    .collection("locations")
+    .collection('locations')
     .find()
     .toArray()
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
-      console.log("location an error occurred", err);
+    .catch((err) => {
+      console.log('location an error occurred', err);
     });
 });
 
-app.get("/activities", (req, res) => {
+app.get('/activities', (req, res) => {
   db.db()
-    .collection("activities")
+    .collection('activities')
     .find()
     .toArray()
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
-      console.log("activities an error occurred", err);
+    .catch((err) => {
+      console.log('activities an error occurred', err);
     });
 });
 
-app.post("/activity", (req, res) => {
+app.post('/activity', (req, res) => {
   const newActivity = req.body.activity;
   db.db()
-    .collection("activities")
+    .collection('activities')
     .insert(newActivity)
-    .then(data => {
-      res.send("inserted with success");
+    .then(() => {
+      res.send('inserted successfully');
     })
-    .catch(error => {
-      console.log(error, "an error occured");
+    .catch((error) => {
+      console.log(error, 'an error occured');
     });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`listening on port ${port}!`));
